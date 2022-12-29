9be80c332198f5ed9832249da9c8608760c41134
KolchinPavel <zibly1984@gmail.com>
2022-12-29 11:43:59 +0300

    new test added

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,7 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello world ");
+
+    }
+    @Test
+    void new_test(){
+        Assertions.assertTrue (3>2);
     }
 }
 
